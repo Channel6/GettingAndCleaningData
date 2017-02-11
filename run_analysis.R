632f658b692d9377f912262af13004b57edbff39
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-02-11 15:56:41 -0800

    redo

--- run_analysis.R
+++ run_analysis.R
@@ -12,80 +12,55 @@ library(dplyr, tidyr, data.table)
 X_train <- X_test <- y_train <- y_test <- NULL
 subject_train <- subject_test <- NULL
 
+# Get required files
+datadir <- "./data"
+if(!file.exists(datadir)){
+	dir.create(datadir)
+}
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(
+	url,
+	destfile = paste(datadir, "/dataset.zip", sep=""),
+	method = "wget",
+	quiet = TRUE
+)
+unzip(
+	zipfile = paste(datadir, "/dataset.zip", sep=""),
+	exdir = datadir
+)
+datafiles <- file.path(datadir, "UCI HAR Dataset")
+files <- list.files(datafiles, recursive = TRUE)	
+
 ## STEP 1: Merge the training and the test sets to create one data set.
 # Read into data frames
-subject_train <- read.table("subject_train.txt")
-subject_test <- read.table("subject_test.txt")
-X_train <- read.table("X_train.txt")
-X_test <- read.table("X_test.txt")
-Y_train <- read.table("y_train.txt")
-Y_test <- read.table("y_test.txt")
+subject_train <- read.table(file.path(datafiles, "train", "subject_train.txt"), header = FALSE)
+subject_test <- read.table(file.path(datafiles, "test", "subject_test.txt"), header = FALSE)
+X_train <- read.table(file.path(datafiles, "train", "X_train.txt"), header = FALSE)
+X_test <- read.table(file.path(datafiles, "test", "X_test.txt"), header = FALSE)
+Y_train <- read.table(file.path(datafiles, "train", "y_train.txt"), header = FALSE)
+Y_test <- read.table(file.path(datafiles, "test", "y_test.txt"), header = FALSE)
+feature_test <- read.table(file.path(datafiles, "test", "X_test.txt"), header = FALSE)
+feature_train <- read.table(file.path(datafiles, "train", "X_train.txt"), header = FALSE)
 
-# Unify name, and clean subject files headers
-names(subject_train) <- "subjectID"
-names(subject_test) <- "subjectID"
+# Concat data tables by rows
+subject <- rbind(subject_train, subject_test)
+activity <- rbind(Y_train, Y_test)
+features <- rbind(X_train, X_test)
 
 # Add column names for measurement files
-featurenames <- read.table("features.txt")
-names(X_train) <- featurenames$V2
-names(X_test) <- featurenames$V2
+featurenames <- read.table(file.path(datafiles, "features.txt"), header = FALSE)
 
-# Get activity levels
-activity_labels <- read.table("activity_labels.txt")
+# Set names to variables
+names(subject) <- c("subject")
+names(activity) <- c("activity")
+names(features) <- featurenames$V2
 
-# Combine files into one dataset
-train <- cbind(subject_train, Y_train, X_train)
-test <- cbind(subject_test, Y_test, X_test)
-combined <- rbind(train, test)
+# Merge columns, create a master frame
+combined <- cbind(subject, activity)
+Data <- cbind(features, combined)
+
 
 ## STEP 2: Extract only the measurements on the mean and standard deviation
 ## 	of each measurement
 
-# Determine which columns contain mean() or std()
-statcolumns <- grepl("mean\\(\\)", names(combined)) || grepl("std\\(\\)", names(combined))
 
-# Keep the subjectID and activity columns
-statcolumns[1:2] <- TRUE
-
-# Clean out irrelevant columns
-combined <- combined[, statcolumns]
-
-## STEP 3: Use descriptive activity names to name the activities
-##	in the data set.
-## STEP 4: Appropriately label the data set with descriptive
-##	activity names.
-
-# Convert the activity column from integer to factor
-combined$activity <- factor(
-			combined$activity,
-			labels=c(
-				"Walking",
-				"Walking Upstairs",
-				"Walking Downstairs",
-				"Sitting",
-				"Standing",
-				"Laying"
-				)
-)
-
-
-## STEP 5: Create a second, independent tidy data set with the
-##	average of each variable for each activity and each subject.
-
-# Create the tidy data set
-melted <- melt(
-		combined,
-		id=c(
-			"subjectID",
-			"activity"
-		)
-)
-
-tidy <- dcast(
-		melted,
-		subjectID+activity ~ variable,
-		mean
-)
-
-# write the tidy data set to a file
-write.csv(tidy, "tidy.csv", row.names=FALSE)
