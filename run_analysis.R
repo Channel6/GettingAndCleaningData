3dae5e839d293485d083d2b3b8960d14edb51798
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-02-11 16:31:11 -0800

    wrap up rebuild

--- run_analysis.R
+++ run_analysis.R
@@ -8,7 +8,7 @@
 ## STEP 0: load required packages
 
 # Init packages and variables
-library(dplyr, tidyr, data.table)
+library(dplyr, tidyr, plyr, data.table)
 X_train <- X_test <- y_train <- y_test <- NULL
 subject_train <- subject_test <- NULL
 
@@ -33,14 +33,70 @@ files <- list.files(datafiles, recursive = TRUE)
 
 ## STEP 1: Merge the training and the test sets to create one data set.
 # Read into data frames
-subject_train <- read.table(file.path(datafiles, "train", "subject_train.txt"), header = FALSE)
-subject_test <- read.table(file.path(datafiles, "test", "subject_test.txt"), header = FALSE)
-X_train <- read.table(file.path(datafiles, "train", "X_train.txt"), header = FALSE)
-X_test <- read.table(file.path(datafiles, "test", "X_test.txt"), header = FALSE)
-Y_train <- read.table(file.path(datafiles, "train", "y_train.txt"), header = FALSE)
-Y_test <- read.table(file.path(datafiles, "test", "y_test.txt"), header = FALSE)
-feature_test <- read.table(file.path(datafiles, "test", "X_test.txt"), header = FALSE)
-feature_train <- read.table(file.path(datafiles, "train", "X_train.txt"), header = FALSE)
+subject_train <- read.table(
+	file.path(
+		datafiles,
+		"train",
+		"subject_train.txt"
+	),
+	header = FALSE
+)
+subject_test <- read.table(
+	file.path(
+		datafiles,
+		"test",
+		"subject_test.txt"
+	),
+	header = FALSE
+)
+X_train <- read.table(
+	file.path(
+		datafiles,
+		"train",
+		"X_train.txt"
+	),
+	header = FALSE
+)
+X_test <- read.table(
+	file.path(
+		datafiles,
+		"test",
+		"X_test.txt"
+	),
+	header = FALSE
+)
+Y_train <- read.table(
+	file.path(
+		datafiles,
+		"train",
+		"y_train.txt"
+	),
+	header = FALSE
+)
+Y_test <- read.table(
+	file.path(
+		datafiles,
+		"test",
+		"y_test.txt"
+	),
+	header = FALSE
+)
+feature_test <- read.table(
+	file.path(
+		datafiles,
+		"test",
+		"X_test.txt"
+	),
+	header = FALSE
+)
+feature_train <- read.table(
+	file.path(
+		datafiles,
+		"train",
+		"X_train.txt"
+	),
+	header = FALSE
+)
 
 # Concat data tables by rows
 subject <- rbind(subject_train, subject_test)
@@ -62,5 +118,54 @@ Data <- cbind(features, combined)
 
 ## STEP 2: Extract only the measurements on the mean and standard deviation
 ## 	of each measurement
+# Get appropriate column names
+subfeaturenames <- featurenames$V2[grep("mean\\(\\)|std\\(\\)", featurenames$V2)]
+# Subset data frame by above subfeaturenames
+selectnames <- c(
+	as.character(subfeaturenames),
+	"subject",
+	"activity"
+)
+Data <- subset(
+	Data,
+	select = selectnames
+)
 
+## STEP 3: Use descriptive activity names to name the activities in the data set
+# read activity names
+activitylabels <- read.table(
+	file.path(
+		datafiles,
+		"activity_labels.txt"
+	),
+	header = FALSE
+)
 
+## STEP 4: Appropriately label the data set with descriptive variable names
+betternames <- list()
+betternames <- c(betternames,
+	list(c("^t", "time")),
+	list(c("^f", "frequency")),
+	list(c("Acc", "Accelerometer")),
+	list(c("Gyro", "Gyroscope")),
+	list(c("Mag", "Magnitude")),
+	list(c("BodyBody", "Body"))
+)
+for(i in 1:length(betternames)){
+	names(Data) <- gsub(betternames[[i]][1], betternames[[i]][2], names(Data))
+}
+
+## STEP 5: Create a second, independent tidy data set with the average of each variable
+##	for each activity and each subject.
+Data2 <- aggregate(. ~subject + activity, Data, mean)
+Data2 <- Data2[
+	order(
+		Data2$subject,
+		Data2$activity
+	),
+]
+write.table(
+	Data2,
+	file = "tidydata.txt",
+	row.name = FALSE
+)
